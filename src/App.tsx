4ac7601d6bcf5e06677028a138d11f1384828870
Viren Patil <virenpatil1@outlook.com>
2021-10-18 16:50:08 +0200

    react router fix

--- src/App.tsx
+++ src/App.tsx
@@ -15,25 +15,20 @@ import CandidatePersonalInfo from './routes/candidate/CandidatePersonalInfo';
 function App() {
   return (
     <Grid container>
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/about' component={About} />
-        <Route exact path='/faq' component={Faq} />
-        <Route exact path='/contact' component={Contact} />
-        <Route exact path='/browse' component={Browse} />
-        <Route exact path='/hr-applications' component={Hr} />
-        <Route exact path='/hr-job-list' component={JobList} />
-        <Route exact path='/hr-personal-info' component={PersonalInfo} />
-        <Route
-          exact
-          path='/candidate-applied-job'
-          component={CandidateAppliedJobs}
-        />
-        <Route
-          path='/candidate-personalInfo'
-          component={CandidatePersonalInfo}
-        />
-      </Switch>
+      <Route exact path='/' component={Home} />
+      <Route exact path='/about' component={About} />
+      <Route exact path='/faq' component={Faq} />
+      <Route exact path='/contact' component={Contact} />
+      <Route exact path='/browse' component={Browse} />
+      <Route exact path='/hr-applications' component={Hr} />
+      <Route exact path='/hr-job-list' component={JobList} />
+      <Route exact path='/hr-personal-info' component={PersonalInfo} />
+      <Route
+        exact
+        path='/candidate-applied-job'
+        component={CandidateAppliedJobs}
+      />
+      <Route path='/candidate-personalInfo' component={CandidatePersonalInfo} />
     </Grid>
   );
 }
