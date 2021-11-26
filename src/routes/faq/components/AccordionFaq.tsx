e4d8469757f42a38855b709b25bc830de7be6d48
Viren Patil <virenpatil1@outlook.com>
2021-11-26 16:46:14 +0100

    Completed FAQ page

---
+++ src/routes/faq/components/AccordionFaq.tsx
@@ -0,0 +1,107 @@
+import React from 'react';
+import { styled } from '@mui/material/styles';
+import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp';
+import MuiAccordion, { AccordionProps } from '@mui/material/Accordion';
+import MuiAccordionSummary, {
+  AccordionSummaryProps,
+} from '@mui/material/AccordionSummary';
+import MuiAccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+// Test
+const Accordion = styled((props: AccordionProps) => (
+  <MuiAccordion disableGutters elevation={0} square {...props} />
+))(({ theme }) => ({
+  border: `1px solid ${theme.palette.divider}`,
+  '&:not(:last-child)': {
+    borderBottom: 0,
+  },
+  '&:before': {
+    display: 'none',
+  },
+}));
+
+const AccordionSummary = styled((props: AccordionSummaryProps) => (
+  <MuiAccordionSummary
+    expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: '0.9rem' }} />}
+    {...props}
+  />
+))(({ theme }) => ({
+  backgroundColor:
+    theme.palette.mode === 'dark'
+      ? 'rgba(255, 255, 255, .05)'
+      : 'rgba(0, 0, 0, 0)',
+  flexDirection: 'row-reverse',
+  '& .MuiAccordionSummary-expandIconWrapper.Mui-expanded': {
+    transform: 'rotate(90deg)',
+  },
+  '& .MuiAccordionSummary-content': {
+    marginLeft: theme.spacing(1),
+  },
+}));
+
+const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
+  padding: theme.spacing(2),
+  borderTop: '1px solid rgba(0, 0, 0, .125)',
+}));
+// TestEnd
+
+const AccordionFaq = () => {
+  const [expanded, setExpanded] = React.useState<string | false>('panel1');
+
+  const handleChange =
+    (panel: string) => (event: React.SyntheticEvent, newExpanded: boolean) => {
+      setExpanded(newExpanded ? panel : false);
+    };
+  return (
+    <div>
+      <Accordion
+        expanded={expanded === 'panel1'}
+        onChange={handleChange('panel1')}
+      >
+        <AccordionSummary aria-controls='panel1d-content' id='panel1d-header'>
+          <h3>What is Joblify?</h3>
+        </AccordionSummary>
+        <AccordionDetails>
+          <h4>
+            Joblify is an Applicant tracking system used by most of the
+            companies who wanted to hire new talent. And save time by reducing
+            the Hr work by making all the applications for the particular job
+            easy to manage and access them.
+          </h4>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion
+        expanded={expanded === 'panel2'}
+        onChange={handleChange('panel2')}
+      >
+        <AccordionSummary aria-controls='panel2d-content' id='panel2d-header'>
+          <h3>
+            Can I integrate it independently for my company?{' '}
+          </h3>
+        </AccordionSummary>
+        <AccordionDetails>
+          <h4>
+            Yes, you can integrate it with your company on your private IP.
+            Contact me for more Information.
+          </h4>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion
+        expanded={expanded === 'panel3'}
+        onChange={handleChange('panel3')}
+      >
+        <AccordionSummary aria-controls='panel3d-content' id='panel3d-header'>
+          <h3>Is it free?</h3>
+        </AccordionSummary>
+        <AccordionDetails>
+          <h4>
+            Joblify is an open source software you don't need to pay anything
+            for the integration. If you want any additional features contact me.
+          </h4>
+        </AccordionDetails>
+      </Accordion>
+    </div>
+  );
+};
+
+export default AccordionFaq;
